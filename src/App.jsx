f437d42094f3ac3cda96714c7966f5bb2b51261c
lilia <ribac.lilia@gmail.com>
2024-07-18 11:39:47 -0500

    add the userInputan and Results Componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import Header from './Components/Header';
+import UserInput from './Components/UserInput';
+import Results from './Components/Results';
+
+import { useState } from 'react';
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChangeInput={handleChange} />
+      <Results input={userInput} />
+    </>
+  );
 }
 
-export default App
+export default App;
